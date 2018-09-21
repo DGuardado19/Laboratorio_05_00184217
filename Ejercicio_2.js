d12d55e60acec11dbdd15b1120733adf5d5ce042
D GUardado <00184217@uca.edu.sv>
2018-09-21 11:17:08 -0600

    ya we :v

--- Ejercicio_2.js
+++ Ejercicio_2.js
@@ -1,8 +1,19 @@
-var cosas =['2','20','>:v PARA ESO ESTA SORT', '5', '1'];
+var cosas =['2','','>:v PARA ESO ESTA SORT', '5', '1'];
 
-for(i=0; i<cosas.length; i++){
-    cosas[i]=
+function ordernar(){
+    var cosas =['2','7','>:v PARA ESO ESTA SORT', '5', '1'];
+    var key;
+    var j;
+    for(var x=1; x<cosas.length;x++){
+        key= cosas[x];
+        j= x-1;
+        while(j>-1 && key<cosas[j]){
+            cosas[j+1]=cosas[j];
+            j=j-1;
+        }
+        cosas[j+1]= key;
+
+    }
+    return cosas;
 }
-
-cosas.sort();
-console.log(cosas);
+console.log(ordernar());
